2656770325919fb87af1975c13ae36a0e81d3140
mewiemers <m.wiemers01@gmail.com>
2021-02-05 11:57:53 +0100

    add operator

--- src/pages/Calculator/calculator.js
+++ src/pages/Calculator/calculator.js
@@ -1,6 +1,7 @@
 import { createElement } from "../../utils/createElement";
 import { createButtonNumber } from "../../components/button/button-number";
 import { createDisplay } from "../../components/display/display";
+import { createButtonOperator } from "../../components/button/button-operator";
 
 export function createCalculator() {
   return createElement("div", {
